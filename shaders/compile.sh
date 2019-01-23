34a717eabfdb91e79bdebbdf7e06a4ee8e62385c
Luzian Bieri <l.j.bieri@gmail.com>
2019-01-23 23:20:36 +0000

    added shader

---
+++ shaders/compile.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/home/lujobi/Documents/coding/sdk/1.1.92.1/x86_64/bin/glslangValidator -V shader.vert
+/home/lujobi/Documents/coding/sdk/1.1.92.1/x86_64/bin/glslangValidator -V shader.frag
